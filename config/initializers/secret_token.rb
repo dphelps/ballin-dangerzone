cbda9846427ef5c19c6d55f50f788f62b359191e
Daniel C Phelps <dphelps@cmu.edu>
2012-11-11 15:28:18 +0300

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Balldin::Application.config.secret_token = '0b39faa73484391d622bbdc668cfbdd9ea0febfb6ef4f7ab441550f1abf85afc3282dfa842b559a18273906a4e4cf703759e7097bf1a060b55fbde7ce2fbde31'
